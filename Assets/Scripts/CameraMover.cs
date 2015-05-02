43c0af8acfb3c5697e6473b2e52cf1ba900ebad5
Zander <zander@uken.com>
2015-05-02 00:21:02 -0400

    Some stuff

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	public bool LookingAtWindow = false;
+	public float lookSpeed = 5f;
+	Quaternion WindowTargetRotation = new Quaternion(0f,0f,0f,1f);
+	Quaternion CounterTargetRotation = new Quaternion(0.4f,0f,0f,0.9f);
+	Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ToggleMode();
+			Debug.Log (transform.rotation);
+		}
+		if (LookingAtWindow) {
+			targetRotation = WindowTargetRotation;
+		} else {
+			targetRotation = CounterTargetRotation;
+		}
+
+		//animationTargetDirection = Vector3.RotateTowards(transform.rotation, targetDirection, lookSpeed * Time.deltaTime, 0.0F);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
+	}
+
+	void ToggleMode() {
+		LookingAtWindow = !LookingAtWindow;
+	}
+}
