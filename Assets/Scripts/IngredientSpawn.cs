6802a82f6a4c4d052026db0b3ead96a5fbf699c6
Renaud Bédard <renaud.b@capybaragames.com>
2015-05-04 12:24:19 -0400

    Better error checking (got this error sometimes?)

--- Assets/Scripts/IngredientSpawn.cs
+++ Assets/Scripts/IngredientSpawn.cs
@@ -19,7 +19,10 @@ public class IngredientSpawn : MonoBehaviour
 			Destroy(lastSpawn);
 
 		var obj = Instantiate(Ingredients.Instance.SnackTemplate, transform.position, Quaternion.identity) as GameObject;
-		obj.GetComponentInChildren<SpriteRenderer>().sprite = Ingredients.Instance.Sprites.First(x => x.name == ingredientName);
+        var matches = Ingredients.Instance.Sprites.Where(x => x.name == ingredientName);
+        if (matches.Count() == 0)   Debug.LogError("No matching sprite for " + ingredientName);
+        if (matches.Count() > 1)    Debug.LogError("Too many matches for " + ingredientName + "(" + matches.Count() + ")");
+        obj.GetComponentInChildren<SpriteRenderer>().sprite = matches.First();
 		obj.transform.parent = transform;
 		obj.transform.localRotation = Quaternion.AngleAxis(90, Vector3.right);
 		obj.GetComponent<Snack>().snackType = ingredientName;
